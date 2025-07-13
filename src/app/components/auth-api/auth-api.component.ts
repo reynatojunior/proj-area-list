7554ecb8469e9a869352ddc551fd55a386976dec
reynatojunior <reynato.junior@hotmail.com>
2025-07-13 19:16:52 -0300

    commit feat :: consumo de posts

--- src/app/components/auth-api/auth-api.component.ts
+++ src/app/components/auth-api/auth-api.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { ServAuthapiService } from '../../services/serv-authapi.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth-api',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './auth-api.component.html',
   styleUrl: './auth-api.component.css'
 })
 export class AuthApiComponent {
+  posts: any[] = [];
+  loading = true;
+  error = false;
 
+  constructor(private apiService: ServAuthapiService) {
+    this.loadPosts();
+  }
+
+  loadPosts(): void {
+    this.apiService.getPosts().subscribe({
+      next: (data) => {
+        this.posts = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = true;
+        this.loading = false;
+      }
+    });
+  }
 }
--- src/app/services/serv-authapi.service.ts
+++ src/app/services/serv-authapi.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServAuthapiService {
+  apiUrl: string = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
